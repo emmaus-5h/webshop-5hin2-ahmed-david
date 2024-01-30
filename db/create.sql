1226ceddcc74841f1db7ccf33f45da58e5282c45
AhmedJ07 <115073507+AhmedJ07@users.noreply.github.com>
2024-01-30 08:58:57 +0000

    feedback GEE

--- db/create.sql
+++ db/create.sql
@@ -2,6 +2,13 @@
 -- create tables
 --
 
+-- feedback GEE 30 jan
+-- je wilt graag de plaatje seerst doen.
+-- besteed er niet teveel tijd aan want dat leevrt niet veel puntne op
+-- in de beoordleingscriteria van de opdracht vind je wat er allemaal in je database moet
+-- video 4 bij de opdracht legt uit hoe je een 1:n relatie toevoegt
+
+
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
@@ -28,3 +35,7 @@ insert into products (name, description, code, price) values ('Chelsea', 'Nam ul
 insert into products (name, description, code, price) values ('Bayern Munchen', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('AC Milan', 'Plane shirt', '6578365796-2', 15);
 insert into products (name, description, code, price) values ('Juventus', 'bus', 'osman-2', 20); 
+insert into products (name, description, code, price) values ('Borussia Dortmund', 'hm', '37438294', 50);
+insert into products (name, description, code, price) values ('Feyenoord', 'fwf', '234234', 100);
+insert into products (name, description, code, price) values ('Brazil', 'tws', '0783', 123);
+insert into products (name, description, code, price) values ('Frankrijk','opq', '1029', 583);
