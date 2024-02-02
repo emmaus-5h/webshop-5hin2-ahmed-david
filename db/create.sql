1f9c010769e6e112efdc69d6aac56b5b19dd7055
AhmedJ07 <115073507+AhmedJ07@users.noreply.github.com>
2024-02-02 13:01:30 +0000

    prijzen

--- db/create.sql
+++ db/create.sql
@@ -27,15 +27,15 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Barcelona', '., rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Real Madrid', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Nederlands Elftal', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Manchester United', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Chelsea', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-insert into products (name, description, code, price) values ('Bayern Munchen', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('AC Milan', 'Plane shirt', '6578365796-2', 15);
-insert into products (name, description, code, price) values ('Juventus', 'bus', 'osman-2', 20); 
-insert into products (name, description, code, price) values ('Borussia Dortmund', 'hm', '37438294', 50);
-insert into products (name, description, code, price) values ('Feyenoord', 'fwf', '234234', 100);
-insert into products (name, description, code, price) values ('Brazil', 'tws', '0783', 123);
-insert into products (name, description, code, price) values ('Frankrijk','opq', '1029', 583);
+insert into products (name, description, code, price) values ('Barcelona', '., rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 99,99);
+insert into products (name, description, code, price) values ('Real Madrid', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 89,99);
+insert into products (name, description, code, price) values ('Nederlands Elftal', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 88);
+insert into products (name, description, code, price) values ('Manchester United', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 99,99);
+insert into products (name, description, code, price) values ('Chelsea', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 92);
+insert into products (name, description, code, price) values ('Bayern Munchen', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 93);
+insert into products (name, description, code, price) values ('AC Milan', 'Plane shirt', '6578365796-2', 90);
+insert into products (name, description, code, price) values ('Juventus', 'bus', 'osman-2', 80); 
+insert into products (name, description, code, price) values ('Borussia Dortmund', 'hm', '37438294', 97);
+insert into products (name, description, code, price) values ('Feyenoord', 'fwf', '234234', 85);
+insert into products (name, description, code, price) values ('Brazil', 'tws', '0783', 99,99);
+insert into products (name, description, code, price) values ('Frankrijk','opq', '1029', 100);
