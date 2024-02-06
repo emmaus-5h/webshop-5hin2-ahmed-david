e3f104e65c4775ec74834349450930e8e933b84f
DavidTabachnik <115638248+DavidTabachnik@users.noreply.github.com>
2024-02-06 08:27:02 +0000

    Shirt beschrijvinge

--- db/create.sql
+++ db/create.sql
@@ -27,15 +27,15 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Barcelona', '., rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 99.99);
-insert into products (name, description, code, price) values ('Real Madrid', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 89.99);
-insert into products (name, description, code, price) values ('Nederlands Elftal', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 88);
-insert into products (name, description, code, price) values ('Manchester United', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 99.99);
-insert into products (name, description, code, price) values ('Chelsea', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 92);
-insert into products (name, description, code, price) values ('Bayern Munchen', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 93);
-insert into products (name, description, code, price) values ('AC Milan', 'Plane shirt', '6578365796-2', 90);
-insert into products (name, description, code, price) values ('Juventus', 'bus', 'osman-2', 80); 
-insert into products (name, description, code, price) values ('Borussia Dortmund', 'hm', '37438294', 97);
-insert into products (name, description, code, price) values ('Feyenoord', 'fwf', '234234', 85);
-insert into products (name, description, code, price) values ('Brazil', 'tws', '0783', 99.99);
-insert into products (name, description, code, price) values ('Frankrijk','opq', '1029', 100);
+insert into products (name, description, code, price) values ('Barcelona', 'Barcelona Retro Thuistenue 1998/99', '816905633-0', 99.99);
+insert into products (name, description, code, price) values ('Real Madrid', 'Real Madrid Retro Thuistenue 1998/99', '077030122-3', 89.99);
+insert into products (name, description, code, price) values ('Nederlands Elftal', 'Nederland Retro Thuistenue 1988', '445924201-X', 88);
+insert into products (name, description, code, price) values ('Manchester United *UCL*', 'Manchester United Retro Thuistenue 2007/08', '686928463-6', 99.99);
+insert into products (name, description, code, price) values ('Chelsea *UCL*', 'Chelsea Retro Thuistenue 2007/08', '492662523-7', 92);
+insert into products (name, description, code, price) values ('Bayern Munchen', 'Bayern Munchen Retro Thuistenue 1996/97', '686928463-6', 93);
+insert into products (name, description, code, price) values ('AC Milan', 'AC Milan Retro Thuistenue 2006/07', '6578365796-2', 90);
+insert into products (name, description, code, price) values ('Juventus', 'Juventus Retro Thuistenue 1996/97', 'osman-2', 80); 
+insert into products (name, description, code, price) values ('Borussia Dortmund', 'Borussia Dortmund Retro Thuistenue 1995/96', '37438294', 97);
+insert into products (name, description, code, price) values ('Feyenoord', 'Feyenoord Retro Thuistenue 2000/01', '234234', 85);
+insert into products (name, description, code, price) values ('Brazil', 'Brazilië Retro Thuistenue 1994', '0783', 99.99);
+insert into products (name, description, code, price) values ('Frankrijk','Frankrijk Retro Uittenue 2006', '1029', 100);
